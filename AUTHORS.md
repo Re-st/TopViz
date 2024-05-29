b65685d1e2c935581532e288b1a7a81ce6b67f5b
Re-st <garypark@kaist.ac.kr>
2024-05-29 10:08:47 +0900

    Add script

---
+++ AUTHORS.md
@@ -0,0 +1,12 @@
+- Original web-app visualizer code by Sang-kil Cha
+  - ``Genealogy Database of Fuzzers''
+  - https://github.com/SoftSec-KAIST/Fuzzing-Survey
+
+- Turned into heatmap visualizer by Ki-hong Heo
+  - ``Haechi Board''
+  - Not yet distributed
+
+- Modification, scripts, and DUG support by Geon Park
+  - ``TopViz''
+  - https://github.com/Re-st/TopViz
+  - PoC : mailto:geon.park@prosys.kaist.ac.kr
---
+++ vis_update_websites.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Copy Heatmap, Web Visualizer, and DUG to the web server
+# In accordance with the Topuzz experiment
+# Copied from $BASE/utils/scripts/vis_update_websites.sh
+
+[ -z "$USER" ] && USER="gun"
+[ -z "$BASE" ] && BASE="$HOME/Topuzz-experiment"
+
+HEATMAPS="$BASE/output/replay-json"
+SP_OUT="$BASE/safe/sparrow-outs"
+DEST="/var/www/html/$USER/dug"
+
+usage() {
+  echo "Usage: $(basename $0) [-h] [-v] [-i]"
+  echo "Options:"
+  echo "  -h  Display this help message"
+  echo "  -v  Update visualizer (.js) only"
+  echo "  -i  Copy the inst-targ map instead of the replay heatmap"
+}
+
+while getopts ":hvi" opt; do
+  case ${opt} in
+  h)
+    usage
+    exit 0
+    ;;
+  v)
+    for REPLAYPATH in $(find $DEST -name "replay.json" -type f); do
+      rsync -az $HOME/TopViz/visualizer $(dirname $REPLAYPATH)
+    done
+    exit 0
+    ;;
+  i)
+    HEATMAPS="$BASE/output/inst-targ-as-replay-json/inst-targ"
+    DEST="/var/www/html/$USER/dug/inst-targ"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [ "$#" -ne 0 ]; then
+  echo "Error: Invalid arguments" >&2
+  usage
+  exit 1
+fi
+
+mkdir -p $SP_OUT
+rsync -az gun@elvis08.kaist.ac.kr:$SP_OUT/ $SP_OUT
+
+mkdir -p $DEST
+rsync -az $HEATMAPS/ $DEST
+
+for REPLAYPATH in $(find $DEST -name "replay.json" -type f); do
+  rsync -az $BASE/utils/visualizer/ $(dirname $REPLAYPATH)
+done
+
+for DUGPATH in $(find $SP_OUT -name "dug.json" -type f); do
+  BIN=$(basename $(dirname $(dirname $DUGPATH)))
+  echo "Processing bin: $BIN"
+  for REPLAYPATH in $(find $DEST/$BIN* -name "replay.json" -type f); do
+    DESTPATH=$(dirname $REPLAYPATH)
+    if [ -f "$DESTPATH/replay.json" ]; then
+      cp $DUGPATH $DESTPATH
+      echo "Copied dug.json to: $DESTPATH"
+    fi
+  done
+done
